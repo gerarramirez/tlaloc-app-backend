11ca52f847666119c874d8d0e55f2b590863a205
Gerardo de Jesus Ramirez Ramos <gramirez>
2024-04-23 21:37:32 -0600

    add dal for Banks products and Commerces

--- tlaloc-api-catalog/dal/banks_produtcs.go
+++ tlaloc-api-catalog/dal/banks_produtcs.go
@@ -1,9 +1,54 @@
 package dal
 
-import model "tlaloc-catalog/model/db"
+import (
+	"errors"
+	model "tlaloc-catalog/model/db"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type BanksProductDao interface {
-	Create(banksProducts *model.BanksProducts) (*model.BanksProducts, error)
+	Create(banksProducts *model.BanksProducts) error
 	FindAll() ([]model.BanksProducts, error)
-	Update(banksProducts *model.BanksProducts) (*model.BanksProducts, error)
+	Update(banksProducts *model.BanksProducts) error
+}
+
+type BanksProducts struct{
+	DB *gorm.DB,
+	GenerateUUID GenerateUUID
+}
+
+func NewBanksProducts(db *gorm.db) * BanksProducts{
+     return &BanksProducts{
+		DB: db,
+		GenerateUUID: func() string{
+            return uuid.New().String()
+		},
+	 }
+}
+
+func(b *BanksProducs) Create(bankProducts *model.BanksProducts) error {
+	if bankProducts == nil{
+		return errors.New("productos bancarios viene vacio")
+	}
+    
+	bpEntity := &model.BanksProductsEntity{
+		BanksProducts: *bankProducts,
+		BaseEntity: model.BaseEntity{
+			ID: b.GenerateUUID,
+			CreatedAt: Time.Now,
+			UpdatedAt: Time.Now,
+		},
+	}
+
+	db := b.DB.Begin()
+	if err := db.Table("").Create(&bpEntity).Error; err!=nil{
+		db.Rollback()
+		return errors.New("error en el guardado de la informacion de producto bancario")
+	}
+    
+
+	return db.Commit().Error
+
 }
